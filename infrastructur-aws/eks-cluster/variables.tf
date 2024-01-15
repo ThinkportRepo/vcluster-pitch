b4e4e50c7ad40d9b54e664e39a0bbdc3630c4ab4
mouadregragui <mregragui@thinkport.digital>
2024-01-15 19:37:33 +0100

    implementing the infrastructure for EKS and install vcluster repository

---
+++ infrastructur-aws/eks-cluster/variables.tf
@@ -0,0 +1,11 @@
+variable "region" {
+  description = "AWS region"
+  type        = string
+  default     = "eu-central-1"
+}
+variable "access_key" {
+  default = "<Your-AWS-Access-Key>"
+}
+variable "secret_key" {
+  default = "<Your-AWS-Secret-Key>"
+}

da3e677165caac36733199e4a9ceaab38eb32069
mouadregragui <mregragui@thinkport.digital>
2024-01-30 23:27:14 +0100

    Fix Calico Plugin and vcluster error

--- infrastructur-aws/eks-cluster-v/modules/kubernetes-helm/calico.tf
+++ infrastructur-aws/eks-cluster-v/modules/kubernetes-helm/calico.tf
@@ -25,7 +25,7 @@ resource "helm_release" "calico" {
   chart            = "tigera-operator"
   namespace        = "tigera-operator"
   create_namespace = true
-  depends_on = [helm_release.v-dev,helm_release.v-admin,helm_release.v-prod]
+  depends_on = [helm_release.v-dev,helm_release.v-admin,helm_release.v-prod,helm_release.prometheus]
   set {
     name  = "kubernetesProvider"
     value = "EKS"
